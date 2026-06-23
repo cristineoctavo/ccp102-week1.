45e3654353dbd47cce5e795fe64ed6b3800b3a7f
cristineoctavo <octavocristineiane8@gmail.com>
2026-06-23 20:10:33 +0800

    Add WEEK 1 Reflection on version control importance

--- ACTIVITY5.py
+++ ACTIVITY5.py
@@ -1,3 +1,7 @@
+WEEK 1 Reflection:
+
+"why do you think version control is important in programming?"
+
 I think version control is important in programming because it helps programmers keep track of the changes they make in their code. 
 If i make mistake or accidentally delete something, I can eazsily go back to an earlier version instead of starting over.
 It also makes teamwork easier since multiple people can work on the same project without over writing each others work.  
