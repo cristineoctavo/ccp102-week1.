44bd2660bd341992d7404d6e3ba64222180600d3
cristineoctavo <octavocristineiane8@gmail.com>
2026-06-25 14:55:46 +0800

    Expand on the importance of version control
    Added thoughts on the usefulness of Git and GitHub for programming students.

--- ACTIVITY5.py
+++ ACTIVITY5.py
@@ -5,3 +5,4 @@ WEEK 1 Reflection:
 I think version control is important in programming because it helps programmers keep track of the changes they make in their code. 
 If i make mistake or accidentally delete something, I can eazsily go back to an earlier version instead of starting over.
 It also makes teamwork easier since multiple people can work on the same project without over writing each others work.  
+For me git and GitHub are useful for programming students because they make it easier to keep track of code changes and prevent losing important work. They also allow students to collaborate on projects, share their code, and learn the same tools that professional programmers use in real-world software development.
